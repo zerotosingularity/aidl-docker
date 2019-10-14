391ad7d7f811d76234e786080aebd6f93d3a79d0
Jan Van de Poel <jan@seeme.ai>
2019-10-14 09:35:53 +0000

    fix latest build

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@
 #docker push zerotosingularity/fastai_v3:1.0.50.post1
 
 export torch_version=1.1.0
-export fastai_version=1.0.57
+export fastai_version=1.0.58
 
 #docker build --build-arg fastai_version=$fastai_version --build-arg torch_version=$torch_version --build-arg tensorflow_version=$tfversion -t zerotosingularity/fastai_v3:$fastai_version .
-docker build --build-arg fastai_version=$fastai_version --build-arg torch_version=$torch_version  -t zerotosingularity/fastai_v3:$fastai_version . --no-cache
+#docker build --build-arg fastai_version=$fastai_version --build-arg torch_version=$torch_version  -t zerotosingularity/fastai_v3:$fastai_version . --no-cache
 docker push zerotosingularity/fastai_v3:$fastai_version
 
-#docker build -t zerotosingularity/fastai_v3:latest . --no-cache
-#docker push zerotosingularity/fastai_v3:latest
+docker build --build-arg fastai_version=$fastai_version --build-arg torch_version=$torch_version  -t zerotosingularity/fastai_v3:latest
+docker push zerotosingularity/fastai_v3:latest
 
 #docker build -t zerotosingularity/aidl-docker:latest .
 #docker push zerotosingularity/aidl-docker-experimental:latest
